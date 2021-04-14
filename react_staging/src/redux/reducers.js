968e19ef0662a1b17dcfe18ef3faa853041ba39f
bxd <649780593@qq.com>
2021-04-14 14:03:39 +0800

    Update reducers.js

--- react_staging/src/redux/reducers.js
+++ react_staging/src/redux/reducers.js
@@ -1,5 +1,6 @@
 // reducer应该是纯函数，不负责业务逻辑，只做单纯的状态管理
 const reducer = (oldState = 0, action) => {
+  // 第一次oldState为undefiend,这里默认为0
   console.log(oldState, action);
   const {type, num} = action
   switch (type) {
